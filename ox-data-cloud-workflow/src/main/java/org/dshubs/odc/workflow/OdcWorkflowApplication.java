fdaa275094aebba3b61dfb2556ba57d89a2eca5b
王贤 <wangxian_drew@163.com>
2022-03-01 14:14:14 +0800

    new add workflow module

---
+++ ox-data-cloud-workflow/src/main/java/org/dshubs/odc/workflow/OdcWorkflowApplication.java
@@ -0,0 +1,16 @@
+package org.dshubs.odc.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangxian
+ */
+@SpringBootApplication
+public class OdcWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdcWorkflowApplication.class, args);
+    }
+
+}
---
+++ ox-data-cloud-workflow/src/test/java/org/dshubs/odc/workflow/OdcWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dshubs.odc.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdcWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
